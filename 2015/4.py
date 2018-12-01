d4aac66db04c20b2a6868fc42587f6815c334203
Pat Myron <pmyron@nd.edu>
2018-12-01 15:19:45 -0500

    2015 4 part 2

--- 2015/4.py
+++ 2015/4.py
@@ -4,6 +4,6 @@ import sys
 input4 = '''bgvyzdsv'''
 
 for i in range(sys.maxsize):
-    if hashlib.md5((input4 + str(i)).encode('utf-8')).hexdigest().startswith("00000"):
+    if hashlib.md5((input4 + str(i)).encode('utf-8')).hexdigest().startswith("000000"):
         print(i)
         exit()
