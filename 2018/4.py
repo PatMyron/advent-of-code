56c17946e76168d7c89fa573f6220b935d814182
Pat Myron <pmyron09@gmail.com>
2018-12-29 01:50:05 -0500

    using one common cache

--- 2018/4.py
+++ 2018/4.py
@@ -5,7 +5,7 @@ import os
 import requests
 import requests_cache
 
-requests_cache.install_cache('cache')
+requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text
 
--- 2018/5.py
+++ 2018/5.py
@@ -3,7 +3,7 @@ import os
 import requests
 import requests_cache
 
-requests_cache.install_cache('cache')
+requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 input = requests.get(url, cookies={"session": os.environ['SESSION']}).text
 
