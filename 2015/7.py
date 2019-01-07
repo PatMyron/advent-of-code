4475235bd544a5139e587e92972686485bf11b58
Pat Myron <pmyron@nd.edu>
2019-01-07 12:07:43 -0500

    WIP: 2015 day 7+8

---
+++ 2015/7.py
@@ -0,0 +1,15 @@
+import re
+import os
+import requests
+import requests_cache
+
+requests_cache.install_cache('cache')
+url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
+s = requests.get(url, cookies={"session": os.environ['SESSION']}).text
+
+values = {}
+for line in s.splitlines():
+    if re.match(r'\d+ -', line):
+        values[str(line.split('->')[1].strip())] = int(line.split('->')[0])
+
+print(values)
---
+++ 2015/8.py
@@ -0,0 +1,11 @@
+import re
+import os
+import requests
+import requests_cache
+
+requests_cache.install_cache('cache')
+url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
+s = requests.get(url, cookies={"session": os.environ['SESSION']}).text
+
+for line in s.splitlines():
+    print(len(repr(line))-len(line)+len(re.findall(r'\\', repr(line))))
