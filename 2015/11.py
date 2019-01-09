b541d673d02839ee80acd299239e22e6a0936940
Pat Myron <pmyron09@gmail.com>
2019-01-08 22:06:14 -0500

    2015 day 11 part 1

--- 2015/11.py
+++ 2015/11.py
@@ -8,9 +8,21 @@ requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
 
+
+def incrementByRecursion(s, i):
+    if s[i] == 'z':
+        s[i] = 'a'
+        incrementByRecursion(s, i - 1)
+    else:
+        s[i] = chr(ord(s[i]) + 1)
+
+
 while True:
     for i in range(len(s) - 2):
-        if s[i:i+3] in string.ascii_lowercase and re.search(r'(\w)\1', s) and not re.search(r'[iol]', s):
+        if s[i:i+3] in string.ascii_lowercase and re.search(r'(\w)\1.*(\w)\2', s) and not re.search(r'[iol]', s):
             print(s)
             exit()
-    # base 26 equivalent of s += 1
+    s = list(s)
+    incrementByRecursion(s, -1)
+    s = ''.join(s)
+
