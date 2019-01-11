dc904cd049fd3f5b84ececfb78b83f5b5ef0c3b4
Pat Myron <pmyron@nd.edu>
2019-01-11 13:57:17 -0500

    2017 day 11 part 2

--- 2017/11.py
+++ 2017/11.py
@@ -11,3 +11,11 @@ ne = input11.split(',').count('ne')-input11.split(',').count('sw')
 se = input11.split(',').count('se')-input11.split(',').count('nw')
 
 print(ne + se)
+
+maximum = 0
+for i in range(len(input11.split(','))):
+    s = abs(input11.split(',')[:i+1].count('s')-input11.split(',')[:i+1].count('n'))
+    ne = abs(input11.split(',')[:i+1].count('ne')-input11.split(',')[:i+1].count('sw'))
+    se = abs(input11.split(',')[:i+1].count('se')-input11.split(',')[:i+1].count('nw'))
+    maximum = max(maximum, ne + se)
+print(maximum)
