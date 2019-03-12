b0adcc9f0d15f4a1599dbfc76619f6f7ecc91543
Pat Myron <pmyron@nd.edu>
2019-03-11 22:51:47 -0400

    fixing ambiguous variable name

--- 2017/6.py
+++ 2017/6.py
@@ -6,23 +6,23 @@ requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
 
-l = list(map(int, s.split()))
+banks = list(map(int, s.split()))
 
 
-def doRound(l):
-    maxIndex = l.index(max(l))
-    maxValue = max(l)
-    l[maxIndex] = 0
+def doRound(banks):
+    maxIndex = banks.index(max(banks))
+    maxValue = max(banks)
+    banks[maxIndex] = 0
     for i in range(maxValue):
-        l[(maxIndex+1+i) % len(l)] += 1
+        banks[(maxIndex + 1 + i) % len(banks)] += 1
 
 
 stored = []
 count = 0
 while True:
-    stored.append(list(l))
-    doRound(l)
+    stored.append(list(banks))
+    doRound(banks)
     count += 1
-    if l in stored:
-        print(count - stored.index(l))
+    if banks in stored:
+        print(count - stored.index(banks))
         break
