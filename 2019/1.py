ad2af8747232b3915ca8316398f9eb684bd19b42
Pat Myron <pmyron@nd.edu>
2019-12-01 11:20:15 -0800

    2019 day 1 part 2

--- 2019/1.py
+++ 2019/1.py
@@ -5,7 +5,11 @@ import requests_cache
 requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
-sum = 0
-for line in s.splitlines():
-    sum += int(line) // 3 - 2
-print(sum)
+sum1, sum2 = 0, 0
+for line in map(int, s.splitlines()):
+    line = line // 3 - 2
+    sum1 += line
+    while line > 0:
+        sum2 += line
+        line = line // 3 - 2
+print(sum1, sum2)
