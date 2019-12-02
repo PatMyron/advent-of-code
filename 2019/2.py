4709872bffde8ba3183439c1b999e75ea3bd3727
Pat Myron <pmyron@nd.edu>
2019-12-01 22:03:42 -0800

    2019 day 2 part 2

--- 2019/2.py
+++ 2019/2.py
@@ -6,18 +6,22 @@ import sys
 requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/2019/day/2/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
-list = list(map(int, s.split(',')))
 
-list[1] = 12
-list[2] = 2
-i = 0
-while True:
-    if list[i] == 99:
-        print(list[0])
-        sys.exit()
-    if list[i] == 1:
-        list[list[i+3]] = list[list[i+1]] + list[list[i+2]]
-        i += 4
-    if list[i] == 2:
-        list[list[i+3]] = list[list[i+1]] * list[list[i+2]]
-        i += 4
+for noun in range(100):
+    for verb in range(100):
+        l = list(map(int, s.split(',')))
+        l[1] = noun
+        l[2] = verb
+        i = 0
+        while True:
+            if l[i] == 99:
+                if l[0] == 19690720:
+                    print(100 * noun + verb)
+                    sys.exit()
+                break
+            elif l[i] == 1:
+                l[l[i+3]] = l[l[i+1]] + l[l[i+2]]
+                i += 4
+            elif l[i] == 2:
+                l[l[i+3]] = l[l[i+1]] * l[l[i+2]]
+                i += 4
