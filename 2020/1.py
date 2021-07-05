b36871a4a5c54f15dfe93dedc45f67cf9b500ca1
Pat Myron <pmyron@nd.edu>
2021-07-04 21:00:23 -0400

    2020 day 1 part 1

---
+++ 2020/1.py
@@ -0,0 +1,13 @@
+import os
+import requests
+import requests_cache
+
+requests_cache.install_cache('../cache')
+url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
+s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
+
+for i in map(int, s.splitlines()):
+    for j in map(int, s.splitlines()):
+        if i + j == 2020:
+            print(i * j)
+            exit()
