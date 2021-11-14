86d2e5e5d5157befad80e08d4307a2f3a7ac5312
Pat Myron <pat.myron@stedi.com>
2021-11-13 19:07:44 -0800

    2016 2 part 1

---
+++ 2016/2.py
@@ -0,0 +1,33 @@
+import os
+import requests
+import requests_cache
+
+requests_cache.install_cache('../cache')
+url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
+s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
+
+position = [1, 1]
+map = {
+    (-1, 1): 1,
+    (0, 1): 2,
+    (1, 1): 3,
+    (-1, 0): 4,
+    (0, 0): 5,
+    (1, 0): 6,
+    (-1, -1): 7,
+    (0, -1): 8,
+    (1, -1): 9,
+}
+for line in s.splitlines():
+    for c in line:
+        if c == 'U':
+            position[1] += 1
+        if c == 'D':
+            position[1] -= 1
+        if c == 'L':
+            position[0] -= 1
+        if c == 'R':
+            position[0] += 1
+        position[0] = sorted((-1, position[0], 1))[1]  # clamp
+        position[1] = sorted((-1, position[1], 1))[1]  # clamp
+    print(map[tuple(position)], end='')
