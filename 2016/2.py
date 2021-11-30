9b1c2369108ca22668ca4972583cbfa6a260af75
Pat Myron <pmyron@nd.edu>
2021-11-30 18:04:56 -0500

    2016 2 part 2

--- 2016/2.py
+++ 2016/2.py
@@ -6,17 +6,21 @@ requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
 
-position = [1, 1]
+position = [0, 2]
 map = {
-    (-1, 1): 1,
-    (0, 1): 2,
-    (1, 1): 3,
-    (-1, 0): 4,
-    (0, 0): 5,
-    (1, 0): 6,
-    (-1, -1): 7,
-    (0, -1): 8,
-    (1, -1): 9,
+    (2, 4): 1,
+    (1, 3): 2,
+    (2, 3): 3,
+    (3, 3): 4,
+    (0, 2): 5,
+    (1, 2): 6,
+    (2, 2): 7,
+    (3, 2): 8,
+    (4, 2): 9,
+    (1, 1): 'A',
+    (2, 1): 'B',
+    (3, 1): 'C',
+    (2, 0): 'D',
 }
 for line in s.splitlines():
     for c in line:
