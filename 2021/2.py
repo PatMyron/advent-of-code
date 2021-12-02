9fff604a8babac7a9d184e18fbac4bbd16a88b10
Pat Myron <PatMyron@users.noreply.github.com>
2021-12-02 00:40:39 -0500

    2021 2 part 2

--- 2021/2.py
+++ 2021/2.py
@@ -6,12 +6,13 @@ requests_cache.install_cache('../cache')
 url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-2] + '/day/' + __file__.split('.')[0] + '/input'
 s = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip()
 
-x, y = 0, 0
+x, y, aim = 0, 0, 0
 for line in s.splitlines():
     if line.startswith('forward'):
+        y += aim * int(line.split()[1])
         x += int(line.split()[1])
     if line.startswith('up'):
-        y += int(line.split()[1])
+        aim -= int(line.split()[1])
     if line.startswith('down'):
-        y -= int(line.split()[1])
+        aim += int(line.split()[1])
 print(x*y)
